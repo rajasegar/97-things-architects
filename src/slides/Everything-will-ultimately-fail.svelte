8ebe315faa4bc8c93a4688704dd56c9c8413a43c
Rajasegar <rajasegar.c@gmail.com>
2022-12-07 15:44:23 +0530

    chore: update everything will fail slide

--- src/slides/Everything-will-ultimately-fail.svelte
+++ src/slides/Everything-will-ultimately-fail.svelte
@@ -27,6 +27,41 @@
 				<p>We are fallible also</p>
 		</Slide>
 		<Slide>
+				<Head2>Automation</Head2>
+				<ul>
+						<li>Removes: The Chance for an error of commission</li>
+						<li>Increases: The Chance for an error of omission</li>
+				</ul>
+				<p>Range of situations</p>
+		</Slide>
+		<Slide>
+				<p>Add monitoring to automation</p>
+				<Head2>More software</Head2>
+				<Head3>More opportunities for failures</Head3>
+		</Slide>
+		<Slide>
+				<Head2>Networks</Head2>
+				<ul>
+						<li>Hardware</li>
+						<li>Software</li>
+						<li>Very long wires</li>
+				</ul>
+		</Slide>
+		<Slide>
+				<Head2>Unpredictable</Head2>
+				<p>State space of a large network &rarr; Infinite</p>
+		</Slide>
+		<Slide>
+				<Head2>Individual components may act deterministically</Head2>
+				<h3 class="bebrush">but still produce essentially chaotic behavior</h3>
+		</Slide>
+		<Slide>
+				<p>Every safety mechanism</p>
+				<Head2>we employ to mitigate one kind of failure</Head2>
+				<Head3>adds new failure modes.</Head3>
+
+		</Slide>
+		<Slide>
 				<Head2>Three Mile Island accident</Head2>
 				<Center>
 						<img src="/assets/images/3-mile-island.webp" alt="Three Mile Island" />
@@ -37,13 +72,18 @@
 				</ul>
 		</Slide>
 		<Slide>
-				<Head2>What can we do about it?</Head2>
 				<p>Faced with the certainty of failure in our systems</p>
+				<Head2>What can we do about it?</Head2>
 		</Slide>
 		<Slide>
-				<Head2>Accept that, no matter what</Head2>
+				<Head2>Accept that</Head2>
 				<p>Your system will have a variety of failure modes</p>
 				<p>Deny that inevitability &rarr; You lose your power to control and contain</p>
+		</Slide>
+		<Slide>
+				<Head2>Once you accept</Head2>
+				<h3>Failures will happen</h3>
+				<p>the ability to design your system's reaction to specific failures</p>
 		</Slide>
 		<Slide>
 				<Head2>Crumple zones</Head2>
@@ -62,7 +102,6 @@
 				<ul>
 						<li>Unpredictable</li>
 						<li>Usually dangerous</li>
-						<li>Happen to emerge</li>
 				</ul>
 		</Slide>
 		<Slide>
