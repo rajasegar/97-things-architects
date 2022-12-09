a489ae7de8f59419477c7528b2131f4cd49f83ed
Rajasegar Chandran <rajasegar.c@gmail.com>
2022-12-09 10:20:30 +0530

    chore: add negotiating slide

--- src/slides/You-are-negotiating.svelte
+++ src/slides/You-are-negotiating.svelte
@@ -1,47 +1,78 @@
 <script>
  import Slide from '../lib/Slide.svelte';
- import Center from '../lib/Center.svelte';
  import Head2 from '../lib/Head2.svelte';
  import Head3 from '../lib/Head3.svelte';
 </script>
 <Slide>
 		<Slide>
 				<Head2>Budgetecture</Head2>
-				<p>Technology choices - Cost cutting</p>
+				<p>Sound Technology choices - Cost cutting</p>
 		</Slide>
+    <Slide>
+        <p>Conversation:</p>
+        <Head3>Do we really need X?</Head3> 
+    </Slide> 
+    <Slide>
+        <Head2>X</Head2>
+        <ul>
+	          <li>Software Licenses</li>
+	          <li>Redundant Servers</li>
+	          <li>Offsite backups</li>
+	          <li>Power supplies</li>
+        </ul>
+
+    </Slide> 
 		<Slide>
 				<Head2>Paternalistic Tone</Head2>
 		</Slide>
 		<Slide>
-				<Head3>Correct answer</Head3>
+				<h3>Correct way to answer</h3>
 				<Head2>Yes we do</Head2>
+        <p>That's almost never the response</p> 
 		</Slide>
 		<Slide>
-				<Head2>Tradeoffs</Head2>
+				<Head3>Engineering is all about making Tradeoffs</Head3>
 				<p>Extravagances</p>
 		</Slide>
 		<Slide>
-				<Head2>Utterly the wrong thing to say</Head2>
+				<Head3>Utterly the wrong thing to say</Head3>
+        <p>Sponsor: Stopped listening right after "Well"</p>
 		</Slide>
 		<Slide>
 				<Head2>Problem</Head2>
-				<p>You see your part as an engineering role</p>
+				<p><span class="font-bold">You:</span>See your part as an engineering role</p>
+        <p><span class="font-bold">Your sponsor:</span> Engaged in Negotiation
+            <hr/>
 				<ul>
-						<li>Collaborative solution-finding exercise</li>
-						<li>Win-lose tactical maneuver</li>
+						<li>We: Collaborative solution-finding exercise</li>
+						<li>They: Win-lose tactical maneuver</li>
 				</ul>
 		</Slide>
 		<Slide>
-				<Head2>Last thing</Head2>
-				<p>Made concessions on the first demand</p>
+				<Head2>Last thing in a negotiation</Head2>
+				<p>Make concessions on the first demand</p>
 		</Slide>
+    <Slide>
+        <h3>Right Response</h3>
+        <Head2>Do we really need?</Head2>
+        
+    </Slide>
 		<Slide>
+        <p>You don't really need 3rd & 4th servers</p> 
 				<Head2>Counter negotiating gambit</Head2>
 				<ul>
+            <li>Get the sponsor to change subject</li>
 						<li>Upping the ante</li>
 						<li>Already running at the bare</li>
 				</ul>
 		</Slide>
+    <Slide>
+        <Head2>Extra Servers</Head2>
+        <ul>
+            <li>QA environment match production</li>
+            <li>Great build box</li>
+        </ul> 
+    </Slide> 
 		<Slide>
 				<Head2>You are negotiating more often than you think</Head2>
 		</Slide>
